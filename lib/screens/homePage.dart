e4b19c12c8245355e68035e5ed1b2aeae1e8a30b
KEREM BAS <kerembas2000@gmail.com>
2022-12-01 13:53:53 +0300

    main

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -2,7 +2,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:deneme/configs/themeColor.dart';
-import 'package:deneme/screens/firstLevel.dart';
+import 'package:deneme/screens/levels/firstLevel.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
